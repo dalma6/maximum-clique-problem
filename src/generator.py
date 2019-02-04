bcc7d05e5627adf5878e4f54e4f96953d03c366d
dalma <dalma@kortechs.io>
2019-02-04 10:47:11 +0100

    add test examples

--- src/generator.py
+++ src/generator.py
@@ -1,6 +1,6 @@
-import random
+import random, json
 
-n = 1000
+n = 50
 branches = [(a, b) for a in range(n) for b in range(n) if a < b and random.random() < 0.25]
 vertices = {}
 for i in range(n):
@@ -8,3 +8,7 @@ for i in range(n):
 
 for (a, b) in branches:
 	vertices[str(a)].append(str(b))
+
+for i in range(15, 25):
+	f = open("input" + str(i) + ".json" ,"w+")
+	json.dump(vertices, f)
