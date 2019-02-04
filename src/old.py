7749a8b0280a30c2c933358ca4c78776fb9a0fea
djordjestanojevic <djordjestanojevic@icloud.com>
2019-02-04 02:59:59 +0100

    update old

--- src/old.py
+++ src/old.py
@@ -1,37 +1,40 @@
 import Graph
-def  clique(U,size):
+def  clique(U , size):
 	global max
-	U.print_graph()
-	if U.length()==0:
-		if size>max:
-			max=size
+	if U.length() == 0:
+		if size > max:
+			max = size
 		return
 	while U.length():
-		if size + U.length()<=max:
+		U.print_graph()
+		if size + U.length() <= max:
 			return
-		min=None
+		min_v = None
 		for v in U.vertices:
-			if min==None or v<min:
-				min=v
+			if min_v == None or int(v) < int(min_v):
+				min_v = v
 		
-		v_i = U.vertices[min]
-		del U.vertices[min]
+		v_i = []
+		for v in U.vertices[min_v]:
+			v_i.append(v)
+
+		del U.vertices[min_v]
 		new_U={}
 		for v in v_i:
 			if v in U.vertices:
-				new_U[v]=[]
+				new_U[v] = []
 				for i in U.vertices[v]:
-					if i in v_i:
+					if i in U.vertices:
 						new_U[v].append(i)
 				if new_U[v]==[]:
 					del new_U[v]
-		U.vertices=new_U			
+		U.vertices=new_U
 		clique(U,size+1)
 	return
 
 
 
-graph = Graph.Graph("input1.json")
+graph = Graph.Graph("input2.json")
 max=0
-clique(graph,0)
+clique(graph,1)
 print max
